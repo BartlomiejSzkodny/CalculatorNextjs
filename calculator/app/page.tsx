740626de44078da7fb35598442cb84f257ff91ab
BartlomiejSzkodny <szkobart@outlook.com>
2024-11-30 23:18:48 +0100

    added div for the display

--- calculator/app/page.tsx
+++ calculator/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from 'react';
 import { Button } from '@mui/material';
+import { blue } from '@mui/material/colors';
 
 export default function Home() {
   const [isClient, setIsClient] = useState(false);
@@ -9,20 +10,26 @@ export default function Home() {
     setIsClient(true);
   }, []);
 
-  const handleClick = (number: number) => {
+  const handleClick = (number: string | number) => {
     console.log(`Button ${number} clicked`);
     // Add your specific logic for each button here
   };
 
   return (
+    <div>
+    
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '10px' }}>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((number) => (
+      
+      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '10px' }}>
+      <div style={{backgroundColor:blue[500], gridColumn: 'span 4',height:50}}></div>
+        {[1, 2, 3,'+', 4, 5, 6,'-', 7, 8, 9, '*','/','%'].map((number) => (
           <Button key={number} variant="contained" onClick={() => handleClick(number)}>
             {number}
           </Button>
         ))}
       </div>
     </div>
+    </div>
+    
   );
 }
